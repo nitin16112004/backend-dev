0cc441e50fc490e37a70971e1c64f9f8f8df562f
Nitin Kumar <nitinkumar16072005@gmail.com>
2026-01-15 14:32:14 +0530

    basic code

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Node.js Backend Started");  // Prints a message to the console indicating the backend has started
+
+let name="nitin";  //Declares a variable 'name' and stores a string value
+let age=24;        //Declares a variable 'age' and stores a numeric value
+const country="India";  //Declares a constant 'country' and stores a string value
+let isstudent=true;   //Declare a variable 'isstudent' and store a boolean value (true/false)
+
+console.log("Name:", name);  //print the value of the variable 'name to the console
+console.log("Age:", age);    //print the value of the variable 'age to the console
+console.log("Country:", country); //print the value of the constant 'country' to the console
+console.log("Is Student:", isstudent);  //print the value of the variable 'isstudent' to the console
