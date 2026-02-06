8af5472f4e6e0900258644296ef83a9542401071
Nitin Kumar <nitinkumar16072005@gmail.com>
2026-02-07 01:15:51 +0530

    [nitin].add add index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+// EX-2
+// import {upperCase, reverseStr, vowelsCount} from "./ex2/stringUtils.js";  //importing the functions from stringUtils.js
+// console.log(vowelsCount("hello my name is nitin kumar"));
+// console.log(upperCase("hello"));
+// console.log(reverseStr("hello"));
+
+// EX-4
+import {createTask, readTask, updateTask} from "./ex4/todo.js";  //importing the functions from todo.js
+// console.log(createTask("nitin kumar",["task1","task2"]));  //creating a task for user nitin with two tasks.
+// console.log(readTask("nitin"));  //reading the task for user nitin.
+// console.log(updateTask("nitin kumar",["nodejs learn","javascript"]));  //updating the task for user nitin by adding a new task.
+// console.log(deleteTask("nitin"));  //deleting the task for user nitin.
