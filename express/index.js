61b0a94ee438ca778bee1b3a39e81f47fb16bce7
Nitin Kumar <nitinkumar16072005@gmail.com>
2026-02-11 16:59:30 +0530

    [nitin].add add the express project

---
+++ express/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import createUser from "./exersice/user.js";
+import filterUsers from "./exersice/ex1.js";
+let port=8080;
+
+const app=express();
+app.use(express.json());
+
+app.post("/users",createUser);
+app.get("/users/:name",filterUsers);
+
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080");
+})
