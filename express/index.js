2d758035c1d9d2bdeb624086f708767ce18fa69e
Nitin Kumar <nitinkumar16072005@gmail.com>
2026-02-11 22:11:10 +0530

    [nitin].add adding ex2.js file

--- express/index.js
+++ express/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import createUser from "./exersice/user.js";
 import filterUsers from "./exersice/ex1.js";
+import responceTime from "./exersice/ex2.js";
 let port=8080;
 
 const app=express();
 app.use(express.json());
+app.use(responceTime);
 
 app.post("/users",createUser);
 app.get("/users/:name",filterUsers);
